ba98f96e177b60a81e17fe415ad6bd44e2c8d1af
david blue <david.w.blue@medtronic.com>
2019-10-18 08:48:28 -0600

    Add project files.

---
+++ PygameModule5/Game/Breakout.py
@@ -0,0 +1,54 @@
+from Game.Shared import *
+
+class Breakout:
+
+    #CTOR
+    def __init__(self):
+        pass 
+
+
+    #public methods
+    def start(self):
+        pass
+
+    def changeScene(self, scene):
+        pass
+
+
+    def getLevel(self):
+        pass
+
+
+    def getScore(self):
+        pass
+
+    def increaseScore(self, score):
+        pass
+
+    def getLives(self):
+        pass
+
+    def getBalls(self):
+        pass
+
+    def getPad(self):
+        pass
+
+    def playSound(self, soundClip):
+        pass
+
+    def reduceLives(self):
+        pass
+
+    def increaseLives(self):
+        pass
+
+    def reset(self):
+        pass
+
+
+
+    #main
+Breakout().start()
+
+
---
+++ PygameModule5/Game/Shared/GameConstants.py
@@ -0,0 +1,3 @@
+class GameConstants:
+    BRICK_SIZE = [100, 30]
+    SCREEN_SIZE = [800, 600]
